7ee77b0201c7403abea652518fa15bdc7de7c766
laintuv00 <laintuv00@gmail.com>
2021-01-09 16:16:48 +0700

    Update rdp-chrome.sh

--- rdp-chrome.sh
+++ rdp-chrome.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 printf "Installing RDP Be Patience... " >&2
 {
+sudo useradd -m ALOK
+sudo adduser ALOK sudo
+echo 'ALOK:123456' | sudo chpasswd
+sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
@@ -13,14 +17,15 @@ sudo systemctl disable lightdm.service
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
-sudo apt install nautilus nano uget -y 
-sudo adduser root chrome-remote-desktop
+sudo apt install nautilus nano -y 
+sudo adduser ALOK chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - root -c """$CRP"""
+su - ALOK -c """$CRP"""
+printf 'ALOK password is 123456 \n'
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
